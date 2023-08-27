f1f10a5cd762cc557abf50dcb640d63baedd676d
Ritik Dwivedi <dwi.ritik20@gmail.com>
2023-08-27 17:27:59 +0530

    some polishment

--- src/components/EditorBody.tsx
+++ src/components/EditorBody.tsx
@@ -6,16 +6,18 @@ import { Editor } from "@monaco-editor/react"
 
 export default function EditorBody() {
     let [selectedLanguage, setSelectedLanguage] = useState('javascript')
-    let [editorTextContent, setEditorTextContent] = useState('')
+    let [editorValue, setEditorValue] = useState('')
     const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
         setSelectedLanguage(e.target.value)
     }
-
+    const handleEditorChange = (newValue: string | undefined, e: ChangeEvent<HTMLElement>) => {
+        setEditorValue(newValue!)
+    }
     return (
         <div className="rounded-md overflow-hidden editor-body">
             <EditorTopBar setSelectChange={handleSelectChange} />
             <div className="editor-main-body">
-                <Editor height="50vh" language={selectedLanguage} theme="vs-dark" value={editorTextContent} />
+                <Editor height="50vh" language={selectedLanguage} theme="vs-dark" value={editorValue} onChange={handleEditorChange} />
             </div>
             <EditorBottomBar />
         </div>

7188ad54ebb3bea6b60903b3e14af01964855a9b
Ritik Dwivedi <dwi.ritik20@gmail.com>
2023-08-27 09:11:58 +0530

    first phase

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from './components/Layout'
+import "./index.css"
+
+function App() {
+  return (
+    <>
+      <Layout />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Layout.tsx
@@ -0,0 +1,8 @@
+import EditorBody from "./EditorBody"
+export default function Layout() {
+    return (
+        <div className="w-2/3 mx-auto mt-12">
+            <EditorBody />
+        </div>
+    )
+}

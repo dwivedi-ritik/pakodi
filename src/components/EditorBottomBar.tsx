cd0085a03718733b1099fbb7ddf24c26b18895d8
Ritik Dwivedi <dwi.ritik20@gmail.com>
2023-08-27 16:18:35 +0530

    integration of language changes

---
+++ src/components/EditorBottomBar.tsx
@@ -0,0 +1,22 @@
+export default function EditorBottomBar() {
+    return (
+        <div className="h-12 mt-4 border-top position-fixed">
+            <div className="flex justify-between items-center">
+                <div className="mt-3 cursor-pointer opacity-50 hover:opacity-100">
+                    <svg className="w-6 h-6 ml-2" fill="none" stroke="currentColor" stroke-width="1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                    </svg>
+                </div>
+                <button className="flex items-center mt-3 mr-3 opacity-50 hover:opacity-100">
+                    <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"></path>
+                    </svg>
+                    <p className="text-xs font-mono">
+                        Clipboard
+                    </p>
+                </button>
+            </div>
+        </div >
+    )
+}
---
+++ src/components/EditorTopBar.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent } from 'react'
+
+interface SelectComponentProps {
+    setSelectChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export default function EditorTopPanel({ setSelectChange }: SelectComponentProps) {
+
+    return (
+        <div className="h-12 border-bottom position-fixed flex justify-between items-center">
+            <p className="text-sm ml-4">
+                Pakcodi <span className="opacity-50 text-xs">(Browser based Text Editor)</span>
+            </p>
+            <div className="flex font-mono text-xs mr-4">
+                <p className="font-medium pr-2">Language</p>
+                <select className=" block w-full bg-gray-700 opacity-75" onChange={setSelectChange}>
+                    <option selected value="javascript">Javascript</option>
+                    <option value="python">Python</option>
+                    <option value="sql">SQL</option>
+                    <option value="java">Java</option>
+                </select>
+            </div>
+        </div>
+    )
+}
